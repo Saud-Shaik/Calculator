7306e8e7c68331895b9e67cd47353b2c383b533e
sk saud <soudshaik18@gmail.com>
2022-01-29 20:34:26 +0530

    first commit

---
+++ Calculator/src/com/sk/ArithmaticCalculator/Calculator1.java
@@ -0,0 +1,35 @@
+package com.sk.ArithmaticCalculator;
+
+import java.util.Scanner;
+public class Calculator1 {
+	
+	
+	   public static void main(String[] args) {
+	      double num1;
+	      double num2;
+	      double ans;
+	      char op;
+	      Scanner sc = new Scanner(System.in);
+	      System.out.print("Enter two numbers: ");
+	      num1 = sc.nextDouble();
+	      num2 = sc.nextDouble();
+	      System.out.print("\nEnter an operator (+, -, *, /): ");
+	      op = sc.next().charAt(0);
+	      switch(op) {
+	         case '+': ans = num1 + num2;
+	            break;
+	         case '-': ans = num1 - num2;
+	            break;
+	         case '*': ans = num1 * num2;
+	            break;
+	         case '/': ans = num1 / num2;
+	            break;
+	      default: System.out.printf("Error! Enter correct operator");
+	         return;
+	      }
+	      System.out.print("\nYour result is:\n");
+	      System.out.printf(num1 + " " + op + " " + num2 + " = " + ans);
+	   }
+	}
+
+
